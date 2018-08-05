60f14e4b615ff29f38afde18c405497237a4ae18
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-08-05 20:15:12 +0100

    Initial commit

---
+++ Authentication.Web.Api/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿namespace Authentication.Web.Api.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get() => new [] { "value1", "value2" };
+    }
+}
